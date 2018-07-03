b424024dd836412b757170c634d14193f13a1340
Tomasz Supel <tomasz.supel@capgemini.com>
2018-07-03 22:50:47 +0200

    PNG

--- mrchecker-app-under-test/src/test/java/com/capgemini/mrchecker/webapi/httpbin/GetImageTest.java
+++ mrchecker-app-under-test/src/test/java/com/capgemini/mrchecker/webapi/httpbin/GetImageTest.java
@@ -7,14 +7,16 @@ import java.io.File;
 import org.junit.Test;
 
 import com.capgemini.mrchecker.test.core.logger.BFLogger;
+import com.capgemini.mrchecker.webapi.BasePageWebApiTest;
 import com.capgemini.mrchecker.webapi.pages.httbin.GetImagePage;
 
 import io.restassured.response.Response;
 
-public class GetImageTest extends com.capgemini.mrchecker.webapi.BasePageWebApiTest {
+public class GetImageTest extends BasePageWebApiTest {
 	
 	private GetImagePage		getImagePage;
 	private final static String	JPEG_FILE_PATH	= "src/test/resources/images/JPG_response.jpg";
+	private final static String	PNG_FILE_PATH	= "src/test/resources/images/PNG_response.png";
 	
 	@Test
 	public void getJpegImage() {
@@ -33,7 +35,29 @@ public class GetImageTest extends com.capgemini.mrchecker.webapi.BasePageWebApiT
 		String encodedFile = getImagePage.encodeFileToBase64(file);
 		String encodedReponse = getImagePage.encodeResponseToBase64(response);
 		
-		assertEquals(encodedFile, encodedReponse);
+		assertEquals("JPEG image in response is different than expected",
+				encodedReponse, encodedFile);
+	}
+	
+	@Test
+	public void getPngImage() {
+		getImagePage = new GetImagePage();
+		
+		BFLogger.logInfo("Step 1 - Sending GET query to " + GetImagePage.PNG_ENDPOINT);
+		Response response = getImagePage.getPngImage();
+		
+		BFLogger.logInfo("Step 2 - Validate response status code and content type:");
+		response.then()
+				.statusCode(200)
+				.contentType("image/png");
+		
+		BFLogger.logInfo("Step 3 - Validate response body:");
+		File file = new File(PNG_FILE_PATH);
+		String encodedFile = getImagePage.encodeFileToBase64(file);
+		String encodedReponse = getImagePage.encodeResponseToBase64(response);
+		
+		assertEquals("PNG image in response is different than expected",
+				encodedReponse, encodedFile);
 	}
 	
 }
