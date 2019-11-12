49af50e01ce908661da37dd62d3327cc2ef74054
Lukasz Stefaniszyn <lukasz.stefaniszyn@capgemini.com>
2019-11-12 18:01:59 +0100

    init Mobile Example Test

---
+++ example/src/main/java/com/capgemini/mrchecker/mobile/simpleapp/SimpleAppPage.java
@@ -0,0 +1,7 @@
+package com.capgemini.mrchecker.mobile.simpleapp;
+
+import com.capgemini.mrchecker.mobile.core.BasePage;
+
+public class SimpleAppPage extends BasePage {
+	
+}
---
+++ example/src/test/java/com/capgemini/mrchecker/mobile/simpleapp/SimpleAppTest.java
@@ -0,0 +1,27 @@
+package com.capgemini.mrchecker.mobile.simpleapp;
+
+import org.junit.Test;
+
+import com.capgemini.mrchecker.test.core.BaseTest;
+
+public class SimpleAppTest extends BaseTest {
+	
+	@Override
+	public void setUp() {
+		// TASK Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void tearDown() {
+		// TASK Auto-generated method stub
+		
+	}
+	
+	@Test
+	public void test() {
+		SimpleAppPage myPage = new SimpleAppPage();
+		
+	}
+	
+}
